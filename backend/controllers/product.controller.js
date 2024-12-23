a40953fe125d1817775b78fec1254740f0b97b68
Joe Destefano <destefanoj380@gmail.com>
2024-12-23 15:42:09 -0800

    `Added cloudinary library and createProduct function to product controller and route`

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,6 @@
 import Product from "../models/product.model.js";
 import { redis } from "../lib/redis.js";
+import cloudinary from "../lib/cloudinary.js";
 
 // Get all products
 export const getAllProducts = async (req, res) => {
@@ -32,5 +33,30 @@ export const getFeaturedProducts = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Error getting featured products", error: error.message });
+    }
+};
+
+// Create a product
+export const createProduct = async (req, res) => {
+    try {
+        const { name, description, price, image, category } = req.body;
+
+        let cloudinaryResponse = null;
+
+        if (image) {
+            await cloudinary.uploader.upload(image, { folder: "products" })
+        }
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+            category
+        })
+        res.status(201).json({ product });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error creating product", error: error.message });
+
     }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
-import { protectRoute } from '../middleware/auth.middleware.js';
-import { adminRoute } from '../middleware/auth.middleware.js';
+import { getAllProducts, getFeaturedProducts, createProduct } from "../controllers/product.controller.js";
+import { protectRoute, adminRoute } from '../middleware/auth.middleware.js';
+
 
 const router = express.Router();
 
 
 router.get("/", protectRoute, adminRoute, getAllProducts)
 router.get("/featured", getFeaturedProducts)
+router.post("/", protectRoute, adminRoute, createProduct)
 
 
 
