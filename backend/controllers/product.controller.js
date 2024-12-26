09a8cf76293edd9f2d1aa54f3a0eed7c1dacc04f
Joe Destefano <destefanoj380@gmail.com>
2024-12-26 15:55:34 -0800

    `Added delete product functionality to product controller and route`

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -60,3 +60,33 @@ export const createProduct = async (req, res) => {
 
     }
 };
+// Delete Product 
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        if (product.image) {
+            const publicId = product.image.split("/").pop().split(".")[0];
+            try {
+                await cloudinary.uploader.destroy(`products/${publicId}`);
+                console.log("Image deleted from Cloudinary");
+            } catch (error) {
+                console.error(error);
+            }
+
+        }
+        const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+        if (!deletedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json({ message: "Product deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error deleting product", error: error.message });
+
+    }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllProducts, getFeaturedProducts, createProduct } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts, createProduct, deleteProduct } from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from '../middleware/auth.middleware.js';
 
 
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get("/", protectRoute, adminRoute, getAllProducts)
 router.get("/featured", getFeaturedProducts)
 router.post("/", protectRoute, adminRoute, createProduct)
-
+router.delete("/:id", protectRoute, adminRoute, deleteProduct)
 
 
 export default router; 
