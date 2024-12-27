5ecff92005499c22bda36aab2e33cc45c542aff6
Joe Destefano <destefanoj380@gmail.com>
2024-12-26 17:31:12 -0800

    `Refactor coupon.controller.js: added user ID and active status checks, updated error handling and responses`

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -3,7 +3,10 @@ import Coupon from "../models/coupon.model.js";
 // Get Coupon
 export const getCoupon = async (req, res) => {
     try {
+        // Fetch the coupon associated with the current user and check if it's active
         const coupon = await Coupon.findOne({ userId: req.user._id, isActive: true });
+
+        // Return the coupon if found, otherwise return null
         res.json(coupon || null);
     } catch (error) {
         console.error("Error getting coupon:", error);
@@ -11,19 +14,25 @@ export const getCoupon = async (req, res) => {
     }
 }
 
-// Validate Coupon 
+// Validate Coupon
 export const validateCoupon = async (req, res) => {
     try {
+        // Extract coupon code from the request body
         const { code } = req.body;
+
+        // Look for the coupon based on the provided code, userId, and active status
         const coupon = await Coupon.findOne({ code, userId: req.user._id, isActive: true });
 
+        // If coupon is not found, return an error message
         if (!coupon) {
             return res.status(404).json({ message: "Coupon not found" });
         }
 
+        // Check if the coupon has expired by comparing expiration date to the current date
         if (coupon.expirationDate < new Date()) {
             coupon.isActive = false;
             await coupon.save();
+
             return res.status(400).json({ message: "Coupon has expired" });
         }
         res.json({
--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,6 @@ app.use("/api/coupon", couponRoutes)
 
 
 
-
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 
