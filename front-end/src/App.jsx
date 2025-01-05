cbf0a08ecccf23995c48e0991ba0efa9b5733623
Joe Destefano <destefanoj380@gmail.com>
2025-01-05 14:07:45 -0800

    `Added navbar component and overlay layout to App.jsx`

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,18 +3,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SignUp from './pages/Signup';
 import Login from './pages/Login';
+import NavBar from './components/Navbar';
+import './styles/global.css';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <div className="container">
+        <div className="overlay">
+          <div className="inner-container">
+            <div className="gradient-background"></div>
+          </div>
+        </div>
+      </div>
+
+      <NavBar />
+
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
-
